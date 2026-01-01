1b4eb74b79c01bfb709b81d751afd28be0b793f4
nick-blip225 <224284647+nick-blip225@users.noreply.github.com>
2026-01-01 06:24:27 -0600

    Add buttons for user input to the webpage
    Add buttons for user inputs.  Add new html divs to display results

--- script.js
+++ script.js
@@ -32,23 +32,30 @@ function getHumanChoice(){
 function playRound (humanChoice, computerChoice){
     if(humanChoice == computerChoice){
         console.log("It was a draw!");
+        //choiceDiv.textContent = "It was a draw!";
     }else if(humanChoice == "Rock" && computerChoice == "Paper"){
         console.log("Loss.  Paper covers rock.");
+        //choiceDiv.textContent = "Loss.  Paper covers rock.";
         computerScore++;
     }else if(humanChoice == "Rock" && computerChoice == "Scissors"){
         console.log("You win!  Rock breaks scissors.");
+        //choiceDiv.textContent = "You win!  Rock breaks scissors.";
         humanScore++;
     }else if(humanChoice == "Paper" && computerChoice == "Rock"){
         console.log("You win!  Paper covers rock.");
+        //choiceDiv.textContent = "You win!  Paper covers rock.";
         humanScore++;
     }else if(humanChoice == "Paper" && computerChoice == "Scissors"){
         console.log("Loss.  Scissors cut paper.");
+        //choiceDiv.textContent = "Loss.  Scissors cut paper.";
         computerScore++;
     }else if(humanChoice == "Scissors" && computerChoice == "Rock"){
         console.log("Loss.  Rock breaks scissors.");
+        //choiceDiv.textContent = "Loss.  Rock breaks scissors.";
         computerScore++;
     }else if(humanChoice == "Scissors" && computerChoice == "Paper"){
         console.log("You win!  Scissors cut paper.");
+        //choiceDiv.textContent = "You win!  Scissors cut paper.";
         humanScore++;
     }
 }
@@ -57,18 +64,38 @@ function playGame(){
     humanScore = 0;
     computerScore = 0;
 
-    for (let index = 0; index < 5; index++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        console.log('The computer chose ' + computerSelection);
+    const body = document.querySelector("body");
+    const rockBtn = document.createElement("button");
+    const paperBtn = document.createElement("button");
+    const scissorsBtn = document.createElement("button");
+    const choiceDiv = document.createElement("div");
+    const scoreDiv = document.createElement("div");
+    const resultDiv = document.createElement("p");
+
+    body.append(rockBtn, paperBtn, scissorsBtn, choiceDiv, scoreDiv, resultDiv);
+
+    rockBtn.textContent = "Rock";
+    paperBtn.textContent = "Paper";
+    scissorsBtn.textContent = "Scissors";
+
+    rockBtn.addEventListener("click", playRound("Rock", getComputerChoice()));
+
+    while (humanScore < 5 && computerScore < 5) {
+
+        playRound(getHumanChoice(), getComputerChoice());
+
+        console.log(`The computer chose  + ${getComputerChoice()}`);
         console.log("Your score: " + humanScore + "\nComputer score: " + computerScore);
+        scoreDiv.textContent = `Your score: ${humanScore}
+                                Computer score: ${computerScore}`
     }
 
     if (humanScore > computerScore) {
-        console.log("You win!");        
+        console.log("You win!");
+        resultDiv.textContent = "You win!";
     }else{
         console.log("Better luck next time.")
+        resultDiv.textContent = "Better luck next time.";
     }
 }
 
